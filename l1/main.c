b3ce60a0f7bbff2354cdf9f91a876cb406cf6674
Denis-26 <kartavenkodenis0@gmail.com>
2017-09-25 10:53:19 +0300

    some changes

--- l1/main.c
+++ l1/main.c
@@ -20,8 +20,8 @@ int main()
             if(fp != NULL) {
                 char str_id[12];
                 sprintf(str_id, "%d", getpid());
-                fprintf(fp,"%s", "Я дочерний процесс\nМой ID: ");
-                fprintf(fp, "%s", strcat(str_id, "\n"));
+                printf("%s", "Я дочерний процесс\nМой ID: ");
+                printf("%s", strcat(str_id, "\n"));
                 for (int i = 0; i < (1024*1024*10)/19; ++i){
                     fputs("Python is the BEST!", fp);
                 }
@@ -31,8 +31,13 @@ int main()
         }
         default:{
             time_t f1 = time(NULL);
+            char str_id[12];
             int status = 0;
+            sprintf(str_id, "%d", getpid());
+            printf("%s", "Я родительский процесс, жду дочерний\nМой ID: ");
+            printf("%s", strcat(str_id, "\n"));
 			waitpid(pid, &status, 0);
+            printf("%s", "Я родительский процесс, дождался\n");
             time_t f2 = time(NULL);
             printf("Time: %ld\n", f2 - f1);
   }
