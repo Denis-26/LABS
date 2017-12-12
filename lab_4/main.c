79c83a48b4f90f6b3371bd9de44085c220edc458
Denis-26 <kartavenkodenis0@gmail.com>
2017-12-12 16:00:06 +0300

    ld

--- lab_4/main.c
+++ lab_4/main.c
@@ -40,7 +40,7 @@ void *myThreadWrite(void *vargp){
     pthread_mutex_lock(&lock);
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    sprintf(time_str, "ID:%lu %d:%d:%d parent time", pthread_self(), tm.tm_hour, tm.tm_min, tm.tm_sec);
+    sprintf(time_str, "ID:%ld %d:%d:%d parent time", pthread_self(), tm.tm_hour, tm.tm_min, tm.tm_sec);
     printf("%s\n", time_str);
     sprintf((char*) (shmem), "%s ", time_str);
     pthread_mutex_unlock(&lock);
