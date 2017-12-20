8252581827454f0d62083853baec9ea3bcaa9e5f
Denis-26 <kartavenkodenis0@gmail.com>
2017-12-20 12:21:58 +0300

    fix

--- lab_5/main.c
+++ lab_5/main.c
@@ -8,86 +8,96 @@
 #include <unistd.h>
 #include <signal.h>
 
-const int SIZE = 512;
+#define SHM_SIZE 128
+
 int shm_id;
 void* shmem;
-int semid = 0 ;
-pthread_t tid[10];
+int create = 1;
+
+int semid;
 struct sembuf sb;
 
-void sig_handler(int sign){
-    if(sign == SIGINT){
+void sig_handler(int signo){
+    if(signo == SIGINT){
         shmdt(shmem);
-        shmctl(shm_id, IPC_RMID, NULL);
+
+        if(!create){
+            shmctl(shm_id, IPC_RMID, NULL);
+            semctl(semid, 0, IPC_RMID);
+        }
+
         exit(0);
     }
 }
 
-void *myThreadRead(){
+void *p_read(){
   while(1){
-    sb.sem_op = -1;
-    if(semop(semid, &sb, 1) == -1)
-      perror("semop error");
-    printf("%s %s\n", (char*) shmem, "child");
-    sb.sem_op = 1;
-    semop(semid, &sb, 1);
+      sb.sem_op = -1;
+      if(semop(semid, &sb, 1) == -1){
+        perror("semop");
+      }
+      printf("%s %s\n", (char*) shmem, "child");
+      sb.sem_op = 1;
+      semop(semid, &sb, 1);
   }
   return NULL;
 }
 
-void *myThreadWrite(){
+void *p_write(){
   char time_str[100] = "";
   while(1){
-    sb.sem_op = -1;
-    semop(semid, &sb, 1);
-    sleep(rand() % 5);
+      sb.sem_op = -1;
+      semop(semid, &sb, 1);
 
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    sprintf(time_str, "ID:%d:%d:%d parent time", tm.tm_hour, tm.tm_min, tm.tm_sec);
-    printf("%s\n", time_str);
+      sleep(1);
 
-    sprintf((char*) (shmem), "%s ", time_str);
-    sb.sem_op = 1;
-    semop(semid, &sb, 1);
+      time_t t = time(NULL);
+      struct tm tm = *localtime(&t);
+      sprintf(time_str, "%d:%d:%d parent time", tm.tm_hour, tm.tm_min, tm.tm_sec);
+      printf("%s\n", time_str);
+      sprintf((char*) (shmem), "%s ", time_str);
+
+      sb.sem_op = 1;
+      semop(semid, &sb, 1);
   }
   return NULL;
 }
 
+int main(int argc, char** argv){
+    setbuf(stdout, NULL);
+    signal(SIGINT, sig_handler);
 
-int main(){
-  //setbuf(stdout, NULL);
-  signal(SIGINT, sig_handler);
-  key_t shm_key = ftok("/dev/random", 1);
-  int exist = 1;
-  shm_id = shmget(shm_key, SIZE, 0);
+    key_t shm_key = ftok("/dev/random", 132);
 
-  if (shm_id == -1){
-    exist = 0;
-    shm_id = shmget(shm_key, SIZE, IPC_CREAT | 0666);
+    shm_id = shmget(shm_key, SHM_SIZE, 0);
     if(shm_id == -1){
-      printf("%s\n", "Error shmget");
+        create = 0;
+        printf("No shared memory\n");
+        shm_id = shmget(shm_key, SHM_SIZE, IPC_CREAT | 0666);
+        if(shm_id == -1){
+            perror("shmget");
+        }
     }
-  }
 
-  shmem = shmat(shm_id, NULL, 0);
-  if (shm_id < 0){
-    printf("%s\n", "attach error");
-    exit(1);
-  }
-  sb.sem_num = 0;
-  sb.sem_flg = 0;
-  if (exist){
-    semid = semget(shm_key, 1, 0660);
-    myThreadRead();
-  } else {
-    semid = semget(shm_key, 1, IPC_CREAT | IPC_EXCL | 0666);
-    semctl(semid, 0, SETVAL, 1);
-    myThreadWrite();
-  }
+    shmem = shmat(shm_id, NULL, 0);
+    if(shmem == (void*)-1){
+        perror("failed to attach shm");
+        exit(-1);
+    }
 
-  shmdt(shmem);
-  shmctl(shm_id, IPC_RMID, NULL);
+    sb.sem_num = 0;
+    sb.sem_flg = 0;
 
-  return 0;
+    if(create){
+        printf("child\n");
+        semid = semget(shm_key, 1, 0660);
+        p_read();
+    }
+    else{
+        printf("parent\n");
+        semid = semget(shm_key, 1, IPC_CREAT | IPC_EXCL | 0666);
+        semctl(semid, 0, SETVAL, 1);
+        p_write();
+    }
+    return 0;
 }
